fa004925fa1b446164caa4846c79baac645ec865
Teerin Leepaiboon <l.teerin@gmail.com>
2023-12-25 16:36:57 +0700

    updata schema

--- src/index.ts
+++ src/index.ts
@@ -44,15 +44,24 @@ clientSource.on('message', async(topic, message) => {
         console.log("Response: ",response)
 
         if(response == "paid"){
-            const order = payload.orderID
+            const order = payload.desc.split(":")[1]
+            console.log("orderId: ",order)
             const trans = await prisma.transactions.update({
                 where: {
-                    order: order
+                    order: order.trim()
                 },
                 data:{
                     jobState: "Accepted"
                 }
             })
+            .then(()=>{
+                console.log("Updated: jobstate")
+            })
+            .catch((err) => {
+                // throw(err.data)
+                console.log("Update: failed: ")
+            })
+            
         }
 
         if(response == "jobstate"){
